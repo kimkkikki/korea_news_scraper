022ee1d6770fb1f9170b070808c5755767cdf2ca
kimkkikki <kimkkikki1@gmail.com>
2017-02-01 14:06:32 +0900

    db conn check

--- news_scraper/pipelines.py
+++ news_scraper/pipelines.py
@@ -22,21 +22,25 @@ class NewsScraperPipeline(object):
         self.curs = self.conn.cursor()
 
     def process_item(self, item, spider):
-        title = item['title']
-        link = item['link']
-        cp = item['cp']
-        self.curs.execute("select * from " + self.database + "." + self.table + " where title = %s or link = %s", (title, link))
-        result = self.curs.fetchone()
+        if self.conn.open:
+            title = item['title']
+            link = item['link']
+            cp = item['cp']
+            self.curs.execute("select * from " + self.database + "." + self.table + " where title = %s or link = %s", (title, link))
+            result = self.curs.fetchone()
 
-        if result:
-            print('data already exist')
+            if result:
+                print('data already exist')
+            else:
+                now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                self.curs.execute("insert into " + self.database + "." + self.table + " (title, link, cp, created_at, updated_at)"
+                                  " values (%s, %s, %s, %s, %s)", (title, link, cp, now, now))
+                self.conn.commit()
+
+            return item
         else:
-            now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            self.curs.execute("insert into " + self.database + "." + self.table + " (title, link, cp, created_at, updated_at)"
-                              " values (%s, %s, %s, %s, %s)", (title, link, cp, now, now))
-            self.conn.commit()
-
-        return item
+            print('db is not connected')
+            print(self.conn.Error)
 
     def spider_closed(self, spider):
         self.conn.close()
