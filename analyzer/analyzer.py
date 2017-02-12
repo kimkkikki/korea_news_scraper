da3712512d597278cf4623d8a7a7dd3f0dab2955
송민정 <minchocosong@gmail.com>
2017-02-12 12:40:50 +0900

    주요값 실행 파라미터로 분리

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -2,6 +2,7 @@ import nltk
 from konlpy.tag import Twitter
 import MySQLdb
 from datetime import datetime, timedelta
+import sys
 
 pos_tagger = Twitter()
 
@@ -9,8 +10,8 @@ pos_tagger = Twitter()
 def tokenize(doc):
     return_array = []
     for t in pos_tagger.pos(doc, norm=True, stem=True):
-       if(t[1] in ('Noun') and t[0] not in candidates):
-           return_array.append('/'.join(t))
+        if t[1] in 'Noun' and t[0] not in candidates:
+            return_array.append('/'.join(t))
 
     return return_array
 
@@ -18,11 +19,18 @@ def tokenize(doc):
 candidates = ['문재인', '안철수', '이재명', '유승민', '안희정', '황교안', '남경필']
 ignore = ['대선', '대행', '행보', '오늘', '권한', '에서']
 
-db = MySQLdb.connect(host="104.199.133.72", user="daesun", passwd="aabb1122", charset="utf8", db="daesun", use_unicode=True)
+host = sys.argv[1]
+db = sys.argv[2]
+user = sys.argv[3]
+passwd = sys.argv[4]
+
+db = MySQLdb.connect(host=host, db=db, user=user, passwd=passwd, charset="utf8", use_unicode=True)
 
 for candidate in candidates:
     cursor = db.cursor()
-    cursor.execute("select title, cp, id, created_at from scraps WHERE created_at between '" + str(datetime.now() - timedelta(hours=3)) + "' and '" + str(datetime.now()) + "' and title like '%" + candidate + "%' ")
+    cursor.execute("select title, cp, id, created_at from scraps WHERE created_at between '" +
+                   str(datetime.now() - timedelta(hours=3)) + "' and '" + str(datetime.now()) +
+                   "' and title like '%" + candidate + "%' ")
 
     recent_data = cursor.fetchall()
 
@@ -33,12 +41,16 @@ for candidate in candidates:
     top_text = {}
 
     for text_data in text.vocab().most_common():
-        if len(text_data[0]) > 1 and text_data[1] > 1 and text_data[1] / len(data_doc) >= 0.3 and (text_data[1] not in ignore):
+        if len(text_data[0]) > 1 and text_data[1] > 1 and text_data[1] / len(data_doc) >= 0.3 \
+                and (text_data[1] not in ignore):
             top_text[text_data[0]] = text_data[1]
 
         if len(top_text) == 3:
             break
 
     for text in top_text:
-        cursor.execute("insert into keywords (candidate, keyword, count, created_at) values (%s,%s,%s, %s)", (candidate, text, top_text[text], str(datetime.now())))
+        cursor.execute("insert into keywords (candidate, keyword, count, created_at) values (%s,%s,%s, %s)",
+                       (candidate, text, top_text[text], str(datetime.now())))
         db.commit()
+
+db.close()
