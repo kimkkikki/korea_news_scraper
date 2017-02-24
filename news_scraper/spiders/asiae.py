2bbd3ac7c2990f306811229efaf29d8f009c8394
kimkkikki <kimkkikki1@gmail.com>
2017-02-24 09:59:04 +0900

    asiae 중복 제거

--- news_scraper/spiders/asiae.py
+++ news_scraper/spiders/asiae.py
@@ -23,7 +23,7 @@ class NewsSpider(scrapy.Spider):
                     title += text
                 title = title.replace('\r', '').replace('\t', '').replace('\n', '').strip()
 
-                if 10 < len(title) < 100:
+                if 10 < len(title) < 100 and '&sec=pol5' not in title:
                     item['title'] = title
                     item['link'] = links[0]
                     item['cp'] = 'asiae'
